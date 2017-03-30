843a84392499a3fa75f8a0156df283b9b138fbd5
Odinei Rocha <odinei.ribeiro@live.com>
2017-03-30 18:19:22 -0300

    added author

--- js/app.js
+++ js/app.js
@@ -1,9 +1,13 @@
-let anedotas = JSON.parse(data);
+let anedotas = JSON.parse(anedotas_json);
+let autores = JSON.parse(autores_json);
+
 let your_anedota_here = document.querySelector('p[name=anedota]');
 
 function get_anedota() {
   let anedota = anedotas.give_me_a_random_one_please();
-  your_anedota_here.innerText = anedota;
+  let autor = autores.give_me_a_random_one_please();
+
+  your_anedota_here.innerText = anedota + " - enviada por " + autor;
 }
 
 Array.prototype.give_me_a_random_one_please = function() {
