c885e8125127448a58332dd554cf74350bb09a6b
Odinei Rocha <odinei.ribeiro@live.com>
2017-03-31 23:55:20 -0300

    added new layout

--- js/app.js
+++ js/app.js
@@ -2,12 +2,14 @@ let anedotas = JSON.parse(anedotas_json);
 let autores = JSON.parse(autores_json);
 
 let your_anedota_here = document.querySelector('p[name=anedota]');
+let your_author_here = document.querySelector('p[name=author]');
 
 function get_anedota() {
   let anedota = anedotas.give_me_a_random_one_please();
   let autor = autores.give_me_a_random_one_please();
 
-  your_anedota_here.innerText = "\"" + anedota + "\"" + "\nenviada por " + autor;
+  your_anedota_here.innerText = anedota;
+  your_author_here.innerText = "\nenviada por " + autor;
 }
 
 Array.prototype.give_me_a_random_one_please = function() {
